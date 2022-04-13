fbd5a397e00e1c8abec68aa21d21aa6a1627670a
sherelene <sherelenedebelen@gmail.com>
2022-04-13 09:44:04 -0700

    ugh

--- main.py
+++ main.py
@@ -1,48 +1,4 @@
-
-def main(input_string):
-    # initializing variables to determine state
-    index = 0
-    character_count = 0
-    # initializing strings to store characters
-    whole_number = ''
-    power = ''
-
-    # checks if the last character in input string is an F or f
-    if input_string[-1] == 'f' or input_string[-1] == 'F':
-        input_string = input_string[:-1]  # deletes f or f from input string if found in last character
-
-    # checks every character in input_string one by one
-    for i in range(len(input_string)):
-        # if "_" is found in input string, it is ignored
-        if "_" == input_string[i]:
-            continue
-        # saves index where e or E is found
-        elif "e" == input_string[i] or "E" == input_string[i]:
-            index = i
-            # puts every number after e into a string to be converted into a float later
-            for j in range(index + 1, len(input_string)):
-                if "_" == input_string[j]:  # if "_" is found in input string, it is ignored
-                    continue
-                else:
-                    power = power + input_string[j]
-            break  # breaks for loop so any number after e won't be included in the whole_number string
-        else:
-            whole_number = whole_number + input_string[i]
-
-        character_count = character_count + 1
-    # if index != character_count then an e was in input_string and needs to be multiplied
-    if index >= character_count:
-        # takes all characters after the index where e was found and converts list to a string to a float
-        #multiplier = float(power)
-        multiplier = convert(power)
-        # takes all characters before the index where e was found and converts list to a string to a float
-        #number = float(whole_number)
-        number = convert(whole_number)
-        num = number * 10 ** multiplier
-        is_int(num)
-    else:
-        is_int(float(whole_number))
-
+def try_again():
     # asks user if they would like to input another string
     retry = input("Would you like to try again? y or n: ")
     if retry == 'Y' or retry == 'y' or retry == 'Yes' or retry == 'yes':
@@ -50,27 +6,81 @@ def main(input_string):
     else:
         return False
 
+def main(input_string):
+    # initializing strings to store characters
+    whole_number = ''
+    decimal_nums = ''
+    power = ''
+    num = [len(input_string)]
+    # initializing variables to determine state
+    i = 0
+    index = 0
+    current_index = 0
+    character_count = 0
+    dot = False
+    last_index = len(input_string) - 1
+    state = "accepted"
+    previous = "q"
 
-# function to print a whole number or a decimal point
-def is_int(x):
-    if x % 1 == 0:
-        print(int(x))
+
+    while i < len(input_string) and state == "accepted":
+        previous_char, num[i], index, state = check_state(previous, input_string[i], i, last_index)
+        i= i + 1
+
+        if state != "accepted":
+           print("sorry that is not a java floating point literal")
+           break
+
+    return try_again()
+
+
+def check_state(previous, character, index, last):
+    # Dictionary of accept states
+    accept_nums = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+                   "7": 7, "8": 8, "9": 9, ".": "."}
+    accept_chars = {"+": "+", "-": "-", "e": "E"}
+    accept_EXT = {"f": "f", "F": "F"}
+
+    if character in accept_nums and pre:
+        return accept_chars[character], index, "accepted"
+    elif character == "_" and index != 0:
+        return "", index, "accepted"
+    elif character in accept_chars and index != 0:
+        return accept_chars[character], index, "accepted"
+    elif character in accept_EXT and index == last:
+        return 0, index, "accepted"
     else:
-        print(x)
+        return 0, index, "not accepted"
+
+    return "not accepted"
 
 
-def convert(characters):
-    # Initialize a variable
-    num = 0
-    n = len(characters)
 
-    # Iterate till length of the string
-    for i in characters:
-        # Subtract 48 from the current digit
-        num = num * 10 + (ord(i) - 48)
+#Dfa for accepting only 001+00101001
+dfa={0:{"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+                   "7": 7, "8": 8, "9": 9, ".": ".", "_": "_"},
+       1:{"e": "E"},
+       2:{"+": "+", "-": "-", "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+          "6": 6, "7": 7, "8": 8, "9": 9},
+       3:{"f": "f", "F": "F"}
 
-        # return the answer
-    return num
+
+
+def accepts(transitions,initial,accepting,s):
+    state = initial
+    try:
+        for c in s:
+            state = transitions[state][c]
+        if(state in accepting):
+            return 'Accepted'
+        else:
+            return 'Rejected'
+    except:
+        return 'Rejected'
+print('Dfa of 101+ ',accepts(dfa,0,{3},'10101111000')) #Accepted
+
+print('Dfa of 001+ ',accepts(dfa,0,{3},'00101010101')) #Accepted
+
 
 
 if __name__ == '__main__':
@@ -78,4 +88,3 @@ if __name__ == '__main__':
     while again:
         user_input = input('Please enter your floating point literal: ')
         again = main(user_input)
-
